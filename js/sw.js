e6cfaea9c35a6fda9e5b73704a72b4fbeb8a0e04
jasim28v-cloud <jasim28v@gmail.com>
2026-04-21 20:23:10 +0300

    Delete js/sw.js

--- js/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'void-lion-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./auth.html','./css/style.css','./js/firebase.js','./js/auth.js','./js/app.js']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
-self.addEventListener('push', e => { const d = e.data?.json() || {title:'VOID LION',body:'إشعار جديد'}; e.waitUntil(self.registration.showNotification(d.title,{body:d.body,icon:'./icon.png'})); });
